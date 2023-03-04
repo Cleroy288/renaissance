ba37636fe97af7950d6f60116bf62b535e0067f4
charles leroy <charlesleroy@MacBook-Air-de-charles-2.local>
2023-03-04 15:33:10 +0100

    push

---
+++ srcs/isalpha/ft_isalpha.c
@@ -0,0 +1,21 @@
+
+/*
+vérifie si le caratère sur le quel on est un cartère , de nature minuscule et / ou majuscule.
+*/
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("%d\n", ft_isalpha(argv[1][0]));
+	}
+}
