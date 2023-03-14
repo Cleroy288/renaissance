7e856c94f873fab06420eca7afe90da3e5d0c380
charles leroy <charlesleroy@MacBook-Air-de-charles-2.local>
2023-03-14 14:31:47 -0400

    push

---
+++ srcs/additional_functions/ft_striteri.c
@@ -0,0 +1,32 @@
+/*
+Parameters
+	s:  The string on which to iterate.
+	f:  The function to apply to each character.
+Description
+	Applies the function ’f’ on each character of
+	the string passed as argument, passing its index
+	as first argument.  Each character is passed by
+	address to ’f’ to be modified if necessary.
+*/
+
+#include <stdio.h>
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	i = -1;
+	if (s && f)
+		while (s[++i])
+			f(i, &s[i]);
+}
+
+void	ft_f(unsigned int pos, char *c)
+{
+	printf("index => [%d], char =>%s\n", pos, c);
+}
+
+int	main()
+{
+	ft_striteri("bonjour", &ft_f);
+}
